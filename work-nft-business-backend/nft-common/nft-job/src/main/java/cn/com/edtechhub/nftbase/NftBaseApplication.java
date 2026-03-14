1b6cc49159ce89e5f25f0765d6bf71cc2d3bc098
limou3434 <898738804@qq.com>
2026-03-14 17:08:20 +0800

    细化项目骨架，并且修复骨架的一些錯漏问题，并且去除一些冗余文件

---
+++ work-nft-business-backend/nft-common/nft-job/src/main/java/cn/com/edtechhub/nftbase/NftBaseApplication.java
@@ -0,0 +1,13 @@
+package cn.com.edtechhub.nftbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NftBaseApplication.class, args);
+    }
+
+}
